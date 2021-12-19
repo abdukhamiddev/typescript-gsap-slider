e7df88e8d8fdcdfe51c6eacc7d46445b19193734
abdukhamiddev <latipovabdukhamid@gmail.com>
2021-12-19 11:22:26 +0500

    completed InfiniteSlider

--- src/InfiniteSlider.ts
+++ src/InfiniteSlider.ts
@@ -41,8 +41,24 @@ export class InfiniteSlider {
     public slide({ collection = [], elemPadding = 0, elemMargin = 0, verticalTranslatePosition = 0, ease = Expo.easeInOut as any, duration = 1 }: SlideParams) {
         this.increaseProgressIndex()
         collection.map((element, index) => {
-
+            const [x, y] = gsap.getProperty(element, 'transform').toString().match(/[0-9\,\-]/g).join('').split(',')
+            const propX = Number(x), propY = Number(y)
+            const propWidth = Number(gsap.getProperty(element, 'width')) + elemPadding + elemMargin;
+            const propXpos = Math.abs(propX)
+            if (propX > 0) {
+                gsap.to(element, {
+                    transform: `translate3d(${propX - propWidth}px,${propY + verticalTranslatePosition}px,0)`,
+                    duration,
+                    ease
+                })
+            } else {
+                gsap.to(element, {
+                    transform: `translate3d(${-(propXpos + propWidth)}px,${propY + verticalTranslatePosition}px,0)`,
+                    ease, duration
+                }).then(() => this.resetBackwards({ collection, propWidth, index }))
+            }
         })
+
 
     }
     public resetBackwards({ collection, propWidth, index }) {
